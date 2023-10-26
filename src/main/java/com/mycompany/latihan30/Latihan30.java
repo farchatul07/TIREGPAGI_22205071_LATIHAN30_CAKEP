6b7964184d2366e42e4fd8385200e3f12ec486b1
farchatul07 <145920038+farchatul07@users.noreply.github.com>
2023-10-26 19:53:03 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/latihan30/Latihan30.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihan30;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author user
+ */
+public class Latihan30 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("\u001b[31m Kamu \u001b[32mngerjain sendiri \u001b[33mlatihan 17 sampe \u001b[34mlatihan 30 ini? \u001b[0m");
+        System.out.print("\u001b[34mJawab \u001b[31m(Yoi/Enggak) \u001b[0m: ");
+        String jawab = scanner.next();
+        if (jawab.equals("Yoi")) {
+            System.out.println("\n\u001b[31mCakep Bener. \u001b[35mGood Job \u001b[0m");
+        }else {
+            System.out.println("""
+                               \n\u001b[31mTetep cakep sih.
+                               \u001b[36mSing penting paham konsep nya yee.\u001b[0m""");
+            System.out.println("Keep the code works dude");
+        }
+    }
+}
